5441f7332ec6754cb260fd2caed2f23136eb9e68
Krzysztof Grochocki <Krzysztof Grochocki@localhost>
2013-08-25 19:51:57 +0200

    * Obs?uga kolorystyki stylizacji okien (barwa i nasycenie). * Drobna poprawka w obs?udze nadawania prze?roczysto?ci obramowa? przez wtyczk? AlphaWindows. * Zmiany optymalizacyjne w kodzie. * Aktualizacja komponent?w AlphaControls do nowszej wersji (8.44).

--- SettingsFrm.cpp
+++ SettingsFrm.cpp
@@ -1,4 +1,25 @@
 //---------------------------------------------------------------------------
+// Copyright (C) 2013 Krzysztof Grochocki
+//
+// This file is part of FixStatusCloud
+//
+// FixStatusCloud is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 3, or (at your option)
+// any later version.
+//
+// FixStatusCloud is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with GNU Radio; see the file COPYING. If not, write to
+// the Free Software Foundation, Inc., 51 Franklin Street,
+// Boston, MA 02110-1301, USA.
+//---------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------
 #include <vcl.h>
 #pragma hdrstop
 #include "SettingsFrm.h"
@@ -25,6 +46,8 @@ __declspec(dllimport)UnicodeString FrendlyFormatJID(UnicodeString JID);
 __declspec(dllimport)bool ChkSkinEnabled();
 __declspec(dllimport)bool ChkThemeAnimateWindows();
 __declspec(dllimport)bool ChkThemeGlowing();
+__declspec(dllimport)int GetSaturation();
+__declspec(dllimport)int GetHUE();
 __declspec(dllimport)void LoadSettings();
 //---------------------------------------------------------------------------
 __fastcall TSettingsForm::TSettingsForm(TComponent* Owner)
@@ -36,12 +59,16 @@ __fastcall TSettingsForm::TSettingsForm(TComponent* Owner)
 void __fastcall TSettingsForm::WMTransparency(TMessage &Message)
 {
   Application->ProcessMessages();
-  sSkinProvider->BorderForm->UpdateExBordersPos(true,(int)Message.LParam);
+  if(sSkinManager->Active) sSkinProvider->BorderForm->UpdateExBordersPos(true,(int)Message.LParam);
 }
 //---------------------------------------------------------------------------
 
 void __fastcall TSettingsForm::FormCreate(TObject *Sender)
 {
+  //Hack na blad w AC
+  #if defined(_WIN64)
+  ContactsListView->SkinData->SkinSection = "";
+  #endif
   //Wlaczona zaawansowana stylizacja okien
   if(ChkSkinEnabled())
   {
@@ -49,12 +76,18 @@ void __fastcall TSettingsForm::FormCreate(TObject *Sender)
 	//Plik zaawansowanej stylizacji okien istnieje
 	if(FileExists(ThemeSkinDir + "\\\\Skin.asz"))
 	{
+	  //Dane pliku zaawansowanej stylizacji okien
 	  ThemeSkinDir = StringReplace(ThemeSkinDir, "\\\\", "\\", TReplaceFlags() << rfReplaceAll);
 	  sSkinManager->SkinDirectory = ThemeSkinDir;
 	  sSkinManager->SkinName = "Skin.asz";
+	  //Ustawianie animacji AlphaControls
 	  if(ChkThemeAnimateWindows()) sSkinManager->AnimEffects->FormShow->Time = 200;
 	  else sSkinManager->AnimEffects->FormShow->Time = 0;
 	  sSkinManager->Effects->AllowGlowing = ChkThemeGlowing();
+	  //Zmiana kolorystyki AlphaControls
+	  sSkinManager->HueOffset = GetHUE();
+	  sSkinManager->Saturation = GetSaturation();
+	  //Aktywacja skorkowania AlphaControls
 	  sSkinManager->Active = true;
 	}
 	//Brak pliku zaawansowanej stylizacji okien
